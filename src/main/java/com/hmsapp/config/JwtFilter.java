363958b917b60c9b5c0a4c19092394817d66e3ef
Aman <amanmomin1307@gmail.com>
2024-12-07 12:50:22 +0530

    06-12-24 1.every URL is sented to filter class to stop that we created addFilterBefore method 2. in JWTFilter added filter chain mechanism

--- src/main/java/com/hmsapp/config/JwtFilter.java
+++ src/main/java/com/hmsapp/config/JwtFilter.java
@@ -1,30 +1,56 @@
 package com.hmsapp.config;
 
+import com.hmsapp.entity.User;
+import com.hmsapp.repository.UserRepository;
 import com.hmsapp.service.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Optional;
 
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private JWTService jwtService;
+    private  UserRepository userRepository;
 
-    public JwtFilter(JWTService jwtService) {
+    public JwtFilter(JWTService jwtService,UserRepository userRepository) {
         this.jwtService = jwtService;
+        this.userRepository = userRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("Authorization");
         System.out.println(token);
-        String jwtToken = token.substring(7);
 
-        String username = jwtService.getUsername(jwtToken);
-        System.out.println(username);
+        if(token!=null && token.startsWith("Bearer")) {
+            String jwtToken = token.substring(7, token.length()-1);
+
+            String username = jwtService.getUsername(jwtToken);
+
+            Optional<User> opUsername = userRepository.findByUsername(username);
+
+            if (opUsername.isPresent()) {
+                User user = opUsername.get();
+
+                UsernamePasswordAuthenticationToken userToken = new UsernamePasswordAuthenticationToken(user, null, null);
+
+                userToken.setDetails(new WebAuthenticationDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(userToken);
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 
 }
--- src/main/java/com/hmsapp/config/SequrityConfig.java
+++ src/main/java/com/hmsapp/config/SequrityConfig.java
@@ -1,13 +1,20 @@
 package com.hmsapp.config;
 
 
+import com.hmsapp.service.JWTService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class SequrityConfig {
+    private JwtFilter jwtFilter;
+
+    public SequrityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain sequrityFilterChain(
@@ -16,6 +23,7 @@ public class SequrityConfig {
 
             //h(cd)2
             http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
+            http.addFilterBefore(jwtFilter, AuthorizationFilter.class);   //WHITHOUT TOKEN REQUESTS I WILL NOT FILTER
             //haap
             http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
