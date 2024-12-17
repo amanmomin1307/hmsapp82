33c828dda94defd803de13744f4f94f4157fe3e8
Aman <amanmomin1307@gmail.com>
2024-12-17 21:29:02 +0530

    16-12-24 1. Created Review entity class and added 2. created ReviewController method it has addReviwes ,viewMyReviews and getUserProfile methods are implemented 3. created review repository

---
+++ src/main/java/com/hmsapp/controller/ReviewController.java
@@ -0,0 +1,60 @@
+package com.hmsapp.controller;
+
+import com.hmsapp.entity.Property;
+import com.hmsapp.entity.Reviews;
+import com.hmsapp.entity.User;
+import com.hmsapp.payload.ProfileDto;
+import com.hmsapp.repository.PropertyRepository;
+import com.hmsapp.repository.ReviewsRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/review")
+public class ReviewController {
+
+    private PropertyRepository propertyRepository;
+    private ReviewsRepository reviewsRepository;
+
+    public ReviewController(PropertyRepository propertyRepository, ReviewsRepository reviewsRepository){
+        this.propertyRepository=propertyRepository;
+        this.reviewsRepository = reviewsRepository;
+    }
+
+    @PostMapping
+    public String addReview(
+            @RequestBody Reviews reviews,
+            @RequestParam long propertyId,
+            @AuthenticationPrincipal User user
+    ){
+        Property property = propertyRepository.findById(propertyId).get();
+        reviews.setProperty(property);
+        reviews.setUser(user);
+        reviewsRepository.save(reviews);
+        return "Added";
+    }
+
+    @GetMapping("user/reviews")
+    public List<Reviews> viewMyReviews(
+            @AuthenticationPrincipal User user
+    ){
+        return reviewsRepository.findByUser(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<ProfileDto> getUserProfile(
+            @AuthenticationPrincipal User user
+    ){
+        ProfileDto dto = new ProfileDto();
+        dto.setUsername(user.getUsername());
+        dto.setEmail(user.getEmail());
+        dto.setUsername(user.getUsername());
+
+        return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/hmsapp/payload/ProfileDto.java
@@ -0,0 +1,13 @@
+package com.hmsapp.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileDto {
+    private String name;
+    private String email;
+    private String username;
+
+}
